580b0a15070c9ce50eb9e979528f7ae26b3779a3
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-16 09:38:28 +0900

    make it more rich !

--- programs/demo.py
+++ programs/demo.py
@@ -2,56 +2,75 @@
 from sklearn.datasets import load_diabetes # 使うライブラリをまとめて宣言しておく
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import matplotlib.widgets as wg
 
-x = load_diabetes().data[:,2].reshape(-1,1) # 今回はライブラリに付属するデータを利用する
+factorid=0
+def changefactor(event):
+    global factorid
+    factorid+=1
+    if factorid>=5:
+        factorid = 0
+    print(factorid)
+
+
+x = load_diabetes().data[:,2].reshape(-1,1)
+factors=[load_diabetes().data[:,i].reshape(-1,1) for i in range(5)]
+
 y = load_diabetes().target
 
-model = LinearRegression() # 線形回帰を行う
-model.fit(x, y) # BMI値から糖尿病の進行を予測する
+models = [LinearRegression() for i in range(5)] # 線形回帰
+##model = LinearRegression()
+list(map(lambda mdl,x:mdl.fit(x,y),models,factors))
+##model.fit(x, y) # BMI値から糖尿病の進行を予測する
 
-##sns.set_style("whitegrid")
+fig, axis = plt.subplots(2,
+                         gridspec_kw=dict(width_ratios=[1],height_ratios=[5,1]),
+                         figsize=(8,5))
 
-# ややこちらの方が慣れているためfigとaxisを使う
-fig, axis = plt.subplots(figsize=(8,5))
+def setup():
+    # ややこちらの方が慣れているためfigとaxisを使う
 
-# 式はグラフ中に描画するのでタイトルをシンプルに
-txt_t = 'Linear Regression Analysis'
-axis.set_title(txt_t,fontsize = 15)
+    # 式はグラフ中に描画するのでタイトルをシンプルに
+    fig.suptitle('Linear Regression Analysis',fontsize = 15)
 
-# 軸ラベル
-axis.set_xlabel("BMI(scaled)",fontsize = 12)
-axis.set_ylabel("Target",fontsize = 12)
+    btn=wg.Button(axis[1],"change factor")
+    btn.on_clicked(changefactor)
 
-#(直線を描画すると描画範囲が変わったので手動で調整)
-axis.set_xlim(-0.18,0.21)
-axis.set_ylim(-50,410)
+    # 軸ラベル
+    axis[0].set_xlabel("BMI(scaled)",fontsize = 12)
+    axis[0].set_ylabel("Target",fontsize = 12)
+
+    #(直線を描画すると描画範囲が変わったので手動で調整)
+    axis[0].set_xlim(-0.18,0.21)
+    axis[0].set_ylim(-50,410)
+
+    #上と右の枠線を消す
+    axis[0].spines["right"].set_visible(False)
+    axis[0].spines[ "top" ].set_visible(False)
+
+
 
 # 式をlatex形式にしてR^2値も追加
-##formula_pos = [0.11,190]
-txt_l = rf'$y = {model.coef_[0]:.1f} x + {model.intercept_:.1f}$'+"\n"+rf'$(R^2 = {model.score(x,y):.3f})$'
-#axis.text(formula_pos[0],formula_pos[1],txt_l)
-
-#上と右の枠線を消す
-axis.spines["right"].set_visible(False)
-axis.spines[ "top" ].set_visible(False)
+#txt_l = rf'$y = {model.coef_[0]:.1f} x + {model.intercept_:.1f}$'+"\n"+rf'$(R^2 = {model.score(x,y):.3f})$'
+txt_l = rf'$y = {models[factorid].coef_[0]:.1f} x + {models[factorid].intercept_:.1f}$'+"\n"+rf'$(R^2 = {models[factorid].score(factors[factorid],y):.3f})$'
 
 #散布図描画(重なっているところが多いので透明度も設定)
-axis.scatter(x,y,alpha=0.5,edgecolors="White")
+#axis[0].scatter(x,y,alpha=0.5,edgecolors="White")
+axis[0].scatter(factors[factorid],y,alpha=0.5,edgecolors="White")
 
 #回帰直線描画
-#axis.plot(x, model.predict(x),color = "black")
-axis.axline((0,model.intercept_),
-            (1,model.coef_[0]),
-            color = "darkred",
-            linestyle = "--",
-            label=txt_l
-            )# 散布図ではなく直線(破線)で表示
+#axis[0].axline(
+#    (0,model.intercept_), (1,model.coef_[0]),
+#    color = "darkred", linestyle = "--", label=txt_l
+#    )# 散布図ではなく直線(破線)で表示
+axis[0].axline(
+    (0,models[factorid].intercept_), (1,models[factorid].coef_[0]),
+    color = "darkred", linestyle = "--", label=txt_l
+    )# 散布図ではなく直線(破線)で表示
 ##sns.regplot(x=x,y=y)
 
 
+axis[0].legend(loc="upper left", borderaxespad=1)
 
-
-
-plt.legend(loc="upper left", borderaxespad=1)
 
 plt.show()
