f1fe2c244f637f1a28ca73d06b2cac06a2ad4f13
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-16 09:59:04 +0900

    .

--- programs/demo.py
+++ programs/demo.py
@@ -13,9 +13,8 @@ def changefactor(event):
     print(factorid)
 
 
-x = load_diabetes().data[:,2].reshape(-1,1)
+#x = load_diabetes().data[:,2].reshape(-1,1)
 factors=[load_diabetes().data[:,i].reshape(-1,1) for i in range(5)]
-
 y = load_diabetes().target
 
 models = [LinearRegression() for i in range(5)] # 線形回帰
@@ -74,3 +73,47 @@ axis[0].legend(loc="upper left", borderaxespad=1)
 
 
 plt.show()
+
+######################################################################3
+
+def main():
+    is_drawing = False
+    
+    factors=[load_diabetes().data[:,i].reshape(-1,1) for i in range(5)]
+    target = load_diabetes().target
+    models = [LinearRegression() for i in range(5)] # 線形回帰
+    list(map(lambda mdl,x:mdl.fit(x,target),models,factors))
+    fig, axis = plt.subplots(
+        2,
+        gridspec_kw=dict(width_ratios=[1],height_ratios=[5,1]),
+        figsize=(8,5)
+    )
+        
+    fig.suptitle('Linear Regression Analysis',fontsize = 15)
+
+    btn=wg.Button(axis[1],"change factor")
+    btn.on_clicked(changefactor)
+
+    # 軸ラベル
+    axis[0].set_xlabel("Factor(scaled)",fontsize = 12)
+    axis[0].set_ylabel("Target",fontsize = 12)
+
+    #(直線を描画すると描画範囲が変わったので手動で調整)
+    axis[0].set_xlim(-0.18,0.21)
+    axis[0].set_ylim(-50,410)
+
+    #上と右の枠線を消す
+    axis[0].spines["right"].set_visible(False)
+    axis[0].spines[ "top" ].set_visible(False)
+    
+    scat = axis[0].scatter(0,0,alpha=0.5,edgecolors="White")
+    line = axis[0].axline((0,0),(0,0),color = "darkred", linestyle = "--", label="")
+    legend = axis[0].legend(loc="upper left", borderaxespad=1)
+
+    while is_drawing:
+        scat.set_offsets(factors[factorid],y)
+        line.set_data((0,models[factorid].intercept_), (1,models[factorid].coef_[0]))
+        legend
+
+if __name__ == "__main__":
+    main()
