e393b8a6e8ca4807e207da7279ff856cf37d6bc8
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-16 11:53:04 +0900

    complete!

--- programs/demo.py
+++ programs/demo.py
@@ -6,50 +6,54 @@ import matplotlib.pyplot as plt
 import matplotlib.widgets as wg
 
 factor_name=[
-    "age","sex","bmi","s1","s2","s3","s4","s5","s6"
+    "Age","Sex","BMI","Blood Pressure","s1","s2","s3","s4","s5","s6"
 ]
+
+factor_num = 10
+factor_id = 2
+txtfunc = ""
 
 def changefactor(event):
     global factor_id
     factor_id+=1
-    if factor_id>=5:
+    if factor_id>=factor_num:
         factor_id = 0
-    print(factor_name[factor_id])
 
 def main():
     is_drawing = True
-    factor_id = 0
     
-    factors=[load_diabetes().data[:,i].reshape(-1,1) for i in range(5)]
+    factors=[load_diabetes().data[:,i].reshape(-1,1) for i in range(factor_num)]
     target = load_diabetes().target
-    models = [LinearRegression() for i in range(5)] # 線形回帰
+    models = [LinearRegression() for i in range(factor_num)] # 線形回帰
     list(map(lambda mdl,x:mdl.fit(x,target),models,factors))
     fig, axis = plt.subplots(
         2,
-        gridspec_kw=dict(width_ratios=[1],height_ratios=[5,1]),
-        figsize=(8,5)
+        gridspec_kw=dict(width_ratios=[1],height_ratios=[1,7]),
+        figsize=(8,6)
     )
         
-    fig.suptitle('Linear Regression Analysis',fontsize = 15)
+    #fig.suptitle('Linear Regression Analysis',fontsize = 15)
+    axis[0].set_title('Linear Regression Analysis',fontsize = 15)
 
-    btn=wg.Button(axis[1],"change factor")
+    btn=wg.Button(axis[0],"")
+    btn.label.set_fontsize(12)
     btn.on_clicked(changefactor)
 
     # 軸ラベル
-    axis[0].set_xlabel("Factor(scaled)",fontsize = 12)
-    axis[0].set_ylabel("Target",fontsize = 12)
+    axis[1].set_xlabel("Factor(scaled)",fontsize = 12)
+    axis[1].set_ylabel("Target",fontsize = 12)
 
     #(直線を描画すると描画範囲が変わったので手動で調整)
-    axis[0].set_xlim(-0.18,0.21)
-    axis[0].set_ylim(-50,410)
+    axis[1].set_xlim(-0.18,0.21)
+    axis[1].set_ylim(-50,410)
 
     #上と右の枠線を消す
-    axis[0].spines["right"].set_visible(False)
-    axis[0].spines[ "top" ].set_visible(False)
+    axis[1].spines["right"].set_visible(False)
+    axis[1].spines[ "top" ].set_visible(False)
     
     #先に型枠だけ作っとく
-    scat = axis[0].scatter([],[],alpha=0.5,edgecolors="White")
-    line = axis[0].axline((0,0),(0,0),color = "darkred", linestyle = "--")
+    scat = axis[1].scatter([],[],alpha=0.5,edgecolors="White")
+    line = axis[1].axline((0,0),(1,0),color = "darkred", linestyle = "--")
     #axis[0].legend(loc="upper left", borderaxespad=1)
     
     
@@ -57,9 +61,11 @@ def main():
         txtfunc=rf'$y = {models[factor_id].coef_[0]:.1f} x + {models[factor_id].intercept_:.1f}$'+"\n"+rf'$(R^2 = {models[factor_id].score(factors[factor_id],target):.3f})$'
 
         scat.set_offsets(list(zip(factors[factor_id],target.reshape(-1,1))))
-        line.set_data((0,models[factor_id].intercept_), (1,models[factor_id].coef_[0]))
+        line.set_xy1((0,models[factor_id].intercept_))
+        line.set_xy2((1,models[factor_id].coef_[0]))
         line.set_label(txtfunc)
-        axis[0].legend(loc="upper left", borderaxespad=1)
+        axis[1].legend(loc="upper left", borderaxespad=1)
+        btn.label.set_text(f"change factor: {factor_name[factor_id]} -> {factor_name[0 if factor_id ==9 else factor_id+1]}")
         
         plt.pause(1)
 
